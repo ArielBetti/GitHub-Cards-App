fa7fe9dbb93b55a2aad8606723906c19bc0567eb
ArielBetti <ariel.betti@gmail.com>
2021-03-22 00:55:48 -0300

    Adicionando react-window

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -6,22 +6,29 @@ import {
   CardImage,
   CardName,
 } from "./style";
+import { FixedSizeList as List } from "react-window";
 
-
-class Card extends React.Component {
-  render() {
-    const profiles = this.props.profiles;
-
-    return (
-      <CardContainer>
-        <CardImage src={profiles.avatar_url} alt="Profile Pic" />
-        <CardContent>
-          <CardName>{profiles.name}</CardName>
-          <CardCompany>{profiles.company}</CardCompany>
-        </CardContent>
-      </CardContainer>
-    );
-  }
+function Card({ profiles }) {
+  const renderRow = ({ index, key, style }) => (
+    <CardContainer key={key}>
+      <CardImage src={profiles[index].avatar_url} alt="Profile Pic" />
+      <CardContent>
+        <CardName>{profiles[index].name}</CardName>
+        <CardCompany>{profiles[index].company}</CardCompany>
+      </CardContent>
+    </CardContainer>
+  );
+  return (
+    <List
+    width={500}
+    height={780}
+    itemCount={profiles.length}
+    itemSize={120}
+    style={{ padding: '15px' }}
+    >
+      {renderRow}
+    </List>
+  );
 }
 
 export default Card;
